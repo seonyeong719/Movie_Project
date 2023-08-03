aa33b338e2ab438e42fdbde02fdc481972ee43e4
seonyeong719 <sy904280@gmail.com>
2023-08-04 02:31:24 +0900

    :sparkles: Feat: home페이지에서 데이터 통신

--- movie/src/Pages/ListPage/home.js
+++ movie/src/Pages/ListPage/home.js
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMovie } from "../../Store/Slice/movieSlice";
+
 function Home() {
-  return <div>home</div>;
+  const [homePages, setHomPages] = useState(1);
+  const dispatch = useDispatch();
+  const getMovieState = useSelector((store) => store.movies);
+
+  console.log(getMovieState);
+
+  useEffect(() => {
+    dispatch(getMovie(homePages));
+  }, []);
+
+  if (getMovieState.loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>Home</div>;
 }
 export default Home;
