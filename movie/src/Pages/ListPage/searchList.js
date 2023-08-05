e15160d10b2407ba4a2ef0a2b75ba6280ebdd0c0
seonyeong719 <sy904280@gmail.com>
2023-08-06 00:06:08 +0900

    :sparkles: Feat: search 페이지 구현 완료

--- movie/src/Pages/ListPage/searchList.js
+++ movie/src/Pages/ListPage/searchList.js
@@ -1,22 +1,54 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getSearchList } from "../../Store/Slice/searchSlice";
+import { styled } from "styled-components";
+import { FlexJustifyCenter } from "../../Styles/common";
+import { useParams } from "react-router-dom";
+import ListCard from "../../Components/Card/listCard";
 
 function SearchList() {
-  const [searchPages, setSearchPages] = useState(1);
+  const { title } = useParams();
   const dispatch = useDispatch();
   const getSearchState = useSelector((store) => store.search);
 
-  console.log(getSearchState);
-
   useEffect(() => {
-    dispatch(getSearchList(searchPages));
+    dispatch(getSearchList({ title }));
   }, []);
 
   if (getSearchState.loading) {
     return <div>Loading...</div>;
   }
 
-  return <div>SearchList</div>;
+  return (
+    <>
+      {getSearchState.search && (
+        <S.Div>
+          {getSearchState.search.results?.length === 0 ? (
+            <div>검색결과가 없다는 페이지</div>
+          ) : (
+            <>
+              <S.List>
+                <ListCard list={getSearchState.search?.results} />
+              </S.List>
+            </>
+          )}
+        </S.Div>
+      )}
+    </>
+  );
 }
 export default SearchList;
+const Div = styled.div`
+  background-color: #111111;
+`;
+
+const List = styled.div`
+  background-color: ${({ theme }) => theme.COLOR.common.black};
+  ${FlexJustifyCenter}
+  flex-wrap: wrap;
+`;
+
+const S = {
+  Div,
+  List,
+};
