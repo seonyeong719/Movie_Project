4225ccdf2f08c0be568e908709012ecc123b3412
seonyeong719 <sy904280@gmail.com>
2023-08-07 16:29:01 +0900

    :sparkles: Feat: 검색 에러 페이지 css 완료

--- movie/src/Components/ErrorPage/errorPage.js
+++ movie/src/Components/ErrorPage/errorPage.js
@@ -1,15 +1,49 @@
 import { styled } from "styled-components";
+import { FlexAllCenter } from "../../Styles/common";
 
 function ErrorPage() {
   return (
-    <Wrap>
-      <div>검색결과가 없습니다</div>
-      <HomeBtn>홈으로 돌아갑니다.</HomeBtn>
-    </Wrap>
+    <S.Wrap>
+      <S.Comment>
+        검색결과가 없습니다 <br /> 홈으로 돌아갑니다
+      </S.Comment>
+      <S.HomeBtn>홈으로 돌아가기</S.HomeBtn>
+    </S.Wrap>
   );
 }
 export default ErrorPage;
 
-const Wrap = styled.div``;
+const Wrap = styled.div`
+  background-color: #111111;
+  width: 100%;
+  height: 100vh;
+  ${FlexAllCenter}
+  text-align: center;
+  flex-direction: column;
+`;
 
-const HomeBtn = styled.button``;
+const Comment = styled.div`
+  color: white;
+  font-size: 1.9rem;
+  line-height: 3rem;
+`;
+
+const HomeBtn = styled.button`
+  width: 20rem;
+  height: 3.5rem;
+  margin-top: 3rem;
+  color: white;
+  font-size: 1.4rem;
+  font-weight: 700;
+  border-radius: 0.4rem;
+  background-color: ${({ theme }) => theme.COLOR.main};
+  cursor: pointer;
+  & :hover {
+  }
+`;
+
+const S = {
+  Wrap,
+  Comment,
+  HomeBtn,
+};
